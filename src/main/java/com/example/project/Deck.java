62d72f513adb635a3877fe51e35a3d8fe05bbb53
Jarian Martinez <jarianm2@nycstudents.net>
2025-02-27 14:31:47 +0000

    wow

--- src/main/java/com/example/project/Deck.java
+++ src/main/java/com/example/project/Deck.java
@@ -15,23 +15,25 @@ public class Deck{
         return cards;
     }
 
-    public  void initializeDeck(){ //hint.. use the utility class
-        
+    public void initializeDeck(){ //hint.. use the utility class
+        for(String suit: Utility.getSuits()) {
+            for(String rank: Utility.getRanks()) {
+                cards.add(new Card(rank, suit));
+            }
+        }
     }
 
     public  void shuffleDeck(){ //You can use the Collections library or another method. You do not have to create your own shuffle algorithm
-        
+        Collections.shuffle(cards);
     }
 
     public  Card drawCard(){
-       return new Card("","");
+        Card draw = cards.get(0);
+        cards.remove(0);
+        return draw;
     }
 
     public  boolean isEmpty(){
         return cards.isEmpty();
     }
-
-   
-
-
 }
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -1,5 +1,6 @@
 package com.example.project;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 
 public class Player{
@@ -16,16 +17,34 @@ public class Player{
     public ArrayList<Card> getAllCards(){return allCards;}
 
     public void addCard(Card c){
-        
+        hand.add(c);
+        allCards.add(c);
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
+    public String playHand(ArrayList<Card> communityCards){   
+        for(int i = 0; i < 3; i++) {
+            allCards.add(communityCards.get(i));
+        }
+        sortCards();
+        ArrayList<Integer> temp1 = new ArrayList<Integer>();
+        ArrayList<String> temp2 = new ArrayList<String>();
+        for(int j = 0; j < 5; j++) {
+            temp1.add(Utility.getRankValue(allCards.get(j).getRank()));
+            temp2.add(allCards.get(j).getSuit());
+        }   
+        int count = 0;
+        for(int temp: temp1) {
+
+        }
         return "Nothing";
     }
 
-    public void SortCards(){} 
+    public void SortAllCards(){
+        allCards.sort(Comparator.comparingInt(eachCard -> Utility.getRankValue(eachCard.getRank())));
+    } 
 
     public ArrayList<Integer> findRankingFrequency(){
+        ArrayList<Integer> freq = new ArrayList<Integer>(13);
         return new ArrayList<>(); 
     }
 
